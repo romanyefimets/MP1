8c8ba0180745518db0ba6d30b7da63849ad5b29c
romanyefimets <romanyefimets@yahoo.com>
2017-10-01 15:50:25 -0700

    fist commit

---
+++ MP1/MP1/Assets/Scripts/DropOption.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropOption : MonoBehaviour {
+
+    public GameObject targetLoc;
+    public Dropdown drop;
+     
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (drop.value != 0)
+        {
+            GameObject shape = Instantiate(Resources.Load(drop.captionText.text, typeof(GameObject))) as GameObject;
+            Renderer shapeRender = shape.GetComponent<Renderer>();
+            Vector3 size = shapeRender.bounds.size;
+            shape.transform.position = new Vector3(targetLoc.transform.position.x, size.y / 2, targetLoc.transform.position.z);
+            drop.value = 0;
+        }
+    }
+}
---
+++ MP1/MP1/Assets/Scripts/delete.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class delete : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit loc;
+
+            if (Physics.Raycast(ray, out loc))
+            {
+                if (loc.collider.gameObject == gameObject)
+                    if (gameObject != null)
+                        Destroy(loc.collider.gameObject);
+            }
+
+        }
+    }
+}
